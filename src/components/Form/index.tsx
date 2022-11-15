12f716033db73347e4a1af53085f3665ac8589ed
Mateus <mateusppa10@gmail.com>
2022-11-15 10:31:42 -0300

    Limitando horas de estudos para 90min no max

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -45,8 +45,8 @@ export const Form = ({setTasks}:{setTasks:React.Dispatch<React.SetStateAction<IT
                     step='1'
                     name='time'
                     id='time'
-                    min='00:00:05'
-                    max='05:00:00'
+                    min='00:10:00'
+                    max='01:30:00'
                     required
                     value={time}
                     onChange={(event)=>setTime(event.target.value)}
